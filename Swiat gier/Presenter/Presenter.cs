37bdec79bb9a1cbff26a9d3661ce0d14ea1a0074
Magdalena Lewek <magda.lewek96@gmail.com>
2019-06-30 11:56:26 +0200

    Proste MVP

---
+++ Swiat gier/Presenter/Presenter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swiat_gier.Presenter
+{
+    class Presenter
+    {
+    }
+}
